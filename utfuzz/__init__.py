0ce1f14fcf36cd3b51e5a717ebae5a94d5f85c6b
Vyacheslav Tamarin <slavabarsuk@ya.ru>
2023-08-11 16:00:52 +0300

    0.1.10: update and fix

---
+++ utfuzz/__init__.py
@@ -0,0 +1,17 @@
+import logging
+import pathlib
+import signal
+import sys
+
+from utfuzz.user_interface.printer import my_print
+
+
+def signal_handler(signal, frame):
+    my_print('\nutfuzz has been cancelled!')
+    sys.exit(0)
+
+
+signal.signal(signal.SIGINT, signal_handler)
+
+FORMAT = '%(asctime)s | %(levelno)s | %(filename)s | %(lineno)d | %(message)s'
+logging.basicConfig(format=FORMAT, filemode='a', filename=pathlib.Path('.utfuzz.log'))
--- utfuzz/parser.py
+++ utfuzz/parser.py
@@ -13,9 +13,9 @@ def parse() -> argparse.Namespace:
     parser.add_argument('--use_config_file', action='store_true', help='Use config file in current directory')
     parser.add_argument('--skip_regression_tests', action='store_true', help='Do not generate regression suite')
     parser.add_argument('-j', '--java', help='Path to Java', default='java')
-    parser.add_argument('-t', '--timeout', type=int, default=60, help='Timeout for test generation process per one '
-                                                                      'class or group of top-level functions from one'
-                                                                      ' file')
+    parser.add_argument('-t', '--timeout', type=int, default=60, help='Timeout in seconds for test generation process '
+                                                                      'per one class or group of top-level functions '
+                                                                      'from one file')
     parser.add_argument('-p', '--project_dir', default='.', help='Root directory with your code for testing (will be '
                                                                  'used for imports and dependencies resolving)')
     parser.add_argument('-o', '--output_dir', default='utbot_tests', help='Directory for generated tests collecting')
@@ -23,7 +23,7 @@ def parse() -> argparse.Namespace:
     parser.add_argument('--sys_paths', nargs='*', default=[], help='Additional path to find imports'
                                                                    '(will be added to `sys.path`, default = project '
                                                                    'directory) [optional]')
-    parser.add_argument('--files_under_test', nargs='*', type=argparse.FileType('r'), default=[],
+    parser.add_argument('--analyze_targets', nargs='*', type=argparse.FileType('r'), default=[],
                         help='List of files or directories for testing, empty means <<test all>> [optional]')
     parser.add_argument('--requirements_file', type=argparse.FileType('r'), help='Path to requirements.txt [optional]')
     return parser.parse_args()
