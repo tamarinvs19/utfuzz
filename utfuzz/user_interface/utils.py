8db2886ea0a722371b1c39312cfdf51308e0fbbd
Vyacheslav Tamarin <slavabarsuk@ya.ru>
2023-08-14 16:12:59 +0300

    Fix java installation dialog

--- utfuzz/user_interface/utils.py
+++ utfuzz/user_interface/utils.py
@@ -1,6 +1,6 @@
 def bool_to_char(b: bool) -> str:
-    return {0: 'n', 1: 'y'}[b]
+    return {False: 'n', True: 'y'}[b]
 
 
 def char_to_bool(c: str, default: bool = True) -> bool:
-    return {'n': 0, 'y': 1, '': default}[c.lower()]
+    return {'n': False, 'y': True, '': default}[c.lower()]
